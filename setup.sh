febe2047666b86c3449f26a4407c88d3d793a346
mrr64 <mrrdev@mrr64.net>
2024-05-08 21:44:56 -0700

    Use --dotfiles option of stow
    Doesn't work for ~/.config directory so this cannont be renamed
    dot-config for now. Version 2.4 will fix but not available on Debian

--- setup.sh
+++ setup.sh
@@ -12,28 +12,32 @@
 
 # directories that should be installed by all users including the root user
 base=(
-  bash
-  bin
-  tmux
-  nvim
-  vim
+	bash
+	bin
+	tmux
+	nvim
+	vim
 )
 
 # directories that should, or only need to be installed for a local user
 useronly=(
-  git
+	git
 )
 
 # run the stow command for the passed in directory ($2) in location ($1)
 
 stowit() {
-  usr=$1
-  app=$2
-  # -v verbose
-  # -R restow (will unstow and then restow)
-  # -t target
+	usr=$1
+	app=$2
+	# -v verbose
+	# -R restow (will unstow and then restow)
+	# -t target
+	# --dotfiles - SUPPOSED to rename dot-* files AND drectories,but current
+	# version does not work with directores...ver 2.4 should fix this but
+	# so far its not available for debian (5/8/24) for now, this only affects
+	# one directory-- ~/.config so this must still use the dot for now.
 
-  stow  -R -t ${usr} ${app}
+	stow --dotfiles -R -t ${usr} ${app}
 
 }
 
@@ -42,16 +46,16 @@ echo "Installing apps for user: ${whoami}"
 
 # install root and local user apps
 for app in ${base[@]}; do
-    echo ${app}
-  stowit "${HOME}" ${app}
+	echo ${app}
+	stowit "${HOME}" ${app}
 done
 
 # install local user only apps
 for app in ${useronly[@]}; do
-  if [[ ! $(id -u) == 0 ]]; then
-    echo ${app}
-    stowit "${HOME}" ${app}
-  fi
+	if [[ ! $(id -u) == 0 ]]; then
+		echo ${app}
+		stowit "${HOME}" ${app}
+	fi
 done
 
 echo ""
